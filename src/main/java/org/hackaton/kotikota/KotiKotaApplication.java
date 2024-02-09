6fb1730c614e2ea102cc1bd6fb3da7f1b6900d57
Mahefaa <hei.mahefa@gmail.com>
2024-02-09 19:18:03 +0300

    chore: init spring-boot gradle project with IDEA

---
+++ src/main/java/org/hackaton/kotikota/KotiKotaApplication.java
@@ -0,0 +1,13 @@
+package org.hackaton.kotikota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotiKotaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotiKotaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hackaton/kotikota/KotiKotaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hackaton.kotikota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotiKotaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
