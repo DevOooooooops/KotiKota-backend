1d47cd6e11436a95b1a24a8bd0624666ab71c6c8
Mahefaa <hei.mahefa@gmail.com>
2024-02-09 22:27:42 +0300

    chore: setup tests, especially CORS test

---
+++ src/test/java/org/hackaton/kotikota/conf/PostgresConf.java
@@ -0,0 +1,22 @@
+package org.hackaton.kotikota.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class PostgresConf {
+  private final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14.9");
+
+  void start() {
+    postgres.start();
+  }
+
+  void stop() {
+    postgres.stop();
+  }
+
+  void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+}
---
+++ src/test/java/org/hackaton/kotikota/integration/mocks/CustomFacadeIT.java
@@ -0,0 +1,19 @@
+package org.hackaton.kotikota.integration.mocks;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import org.hackaton.kotikota.conf.FacadeIT;
+import org.hackaton.kotikota.endpoint.rest.security.auth.firebase.FirebaseAuthenticator;
+import org.hackaton.kotikota.endpoint.rest.security.auth.firebase.FirebaseConf;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+public class CustomFacadeIT extends FacadeIT {
+    @MockBean
+    protected FirebaseConf firebaseConfMock;
+    @MockBean
+    protected FirebaseApp firebaseAppMock;
+    @MockBean
+    protected FirebaseAuth firebaseAuthMock;
+    @MockBean
+    protected FirebaseAuthenticator firebaseAuthenticator;
+}
